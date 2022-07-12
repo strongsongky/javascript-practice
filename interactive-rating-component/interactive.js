20dae10cf0899526e05a85d9a4b858ab810024a2
용이 <thdruddyd510@naver.com>
2022-07-13 00:09:41 +0900

    interactive.js-failed

--- interactive-rating-component/interactive.js
+++ interactive-rating-component/interactive.js
@@ -1,11 +1,9 @@
 const submitButton = document.getElementsByClassName("submit")[0];
 const radioInputs = document.querySelectorAll('input[name="ratings"]');
 let result;
-
 for (let i = 0; i < radioInputs.length; i++) {
   radioInputs[i].addEventListener("click", function () {
     result = radioInputs[i].value;
-    console.log("result", result);
   });
 }
 
@@ -24,10 +22,48 @@ function handleSubmit() {
   selectionMention.appendChild(pTag);
 }
 
+submitButton.addEventListener("click", function () {
+  radioInputs.checked = true;
+});
+
+const checked = document.querySelectorAll("input[type=radio]:checked");
+
+function check() {
+  if (checked.length > 0) {
+    submitButton.removeAttribute("disabled");
+  }
+}
+
 submitButton.onclick = function () {
   handleSubmit();
+  check();
 };
 
-let choosenum = document.getElementsByClassName("rating-numbers");
-let letters = document.getElementsByClassName("selection-letters")[0];
-let hidden = document.getElementsByClassName("rating-state hidden")[0];
+/*radioInputs.forEach(function (el) {
+  el.addEventListener("click", function () {
+    checked = document.querySelectorAll("input[type=radio]:checked");
+    if (checked.length) {
+      submitButton.removeAttribute("disabled");
+    }
+  });
+});*/
+
+/*function check() {
+  if (radioInputs.checked) {
+    submitButton.removeAttribute("disabled");
+  }
+}*/
+
+/*submitButton.addEventListener("click", function () {
+  if (radioInputs.checked == true) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+});*/
+
+/* if(라디오버튼이 체크가 하나도 안돼있으면) {
+  submit버튼을 disalbe로 바꾸기
+} else {
+  원래대로
+}*/
